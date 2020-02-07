d464c87a7f8e55644a4db0cf0659cc77864b53c3
Ben Hickson <bzhickson@gmail.com>
2020-02-07 05:04:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,5 +30,15 @@ def using_insert(array, element)
   array.insert(4, element)
 end
 
+def using_uniq(array)
+  array.uniq
+end
 
+def using_flaten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
   
